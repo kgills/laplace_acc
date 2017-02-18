4187ea5b4abfde3eed9337173cc5a3fc0e63c182
Kevin Gillespie <kgills@gmail.com>
2017-02-17 23:19:06 -0600

    Adding report and graphs.

--- scripts/graphs.r
+++ scripts/graphs.r
@@ -1,49 +1,55 @@
-library(Hmisc)
+library(gridExtra)
 
-k80_2048 = read.csv("../data/k80_2048_4.txt")
-k80_4096 = read.csv("../data/k80_4096_4.txt")
-k80_8192 = read.csv("../data/k80_8192_4.txt")
-k80_16384 = read.csv("../data/k80_16384_4.txt")
+k80_all = read.csv("../data/k80_all.txt")
+p100_all = read.csv("../data/p100_all.txt")
 
 # Convert FLOPS to GFLOPS
-k80_2048$flops = k80_2048$flops /1000000000
-k80_4096$flops = k80_4096$flops /1000000000
-k80_8192$flops = k80_8192$flops /1000000000
-k80_16384$flops = k80_16384$flops /1000000000
-
-# TODO: Add the serial dataframes
+k80_all$flops = k80_all$flops /1000000000
+p100_all$flops = p100_all$flops /1000000000
 
 # Aggregate the cores and mean flops
-core_flops_ag = aggregate(flops~cores, core_all, mean)
+k80_all_ag = aggregate(flops~matrix_dim, k80_all, mean)
+p100_all_ag = aggregate(flops~matrix_dim, p100_all, mean)
+
+# png("k80_all_ag.png") 
+# grid.table(k80_all_ag, rows=NULL)
+# dev.off()
+
+# png("p100_all_ag.png") 
+# grid.table(p100_all_ag, rows=NULL)
+# dev.off()
+
+
+# Add the serial execution data
+serial_16384 = read.csv("../data/serial_16384.txt")
+k80_16384 = read.csv("../data/k80_16384_4.txt")
+p100_16384 = read.csv("../data/p100_16384.txt")
+
+k80_16384=rbind(k80_16384, serial_16384)
+p100_16384=rbind(p100_16384, serial_16384)
+
+k80_16384<-k80_16384[order(k80_16384$cores),]
+p100_16384<-p100_16384[order(p100_16384$cores),]
+
+k80_16384_ag=aggregate(flops~cores,k80_16384, mean)
+p100_16384_ag=aggregate(flops~cores,p100_16384, mean)
 
 # Add the speedup
-core_flops_ag$speedup = core_flops_ag$flops/core_flops_ag$flops[1]
+k80_16384_ag$speedup = k80_16384_ag$flops/k80_16384_ag$flops[1]
+p100_16384_ag$speedup = p100_16384_ag$flops/p100_16384_ag$flops[1]
 
-# Add the serial portion
-core_flops_ag$serial = (core_flops_ag$speedup - core_flops_ag$cores)/(1 - core_flops_ag$cores)
+# Add the serial
+k80_16384_ag$serial = (k80_16384_ag$speedup - k80_16384_ag$cores)/(1 - k80_16384_ag$cores)
+p100_16384_ag$serial = (p100_16384_ag$speedup - p100_16384_ag$cores)/(1 - p100_16384_ag$cores)
 
-# Add the efficiency 
-# core_flops_ag$efficiency = (((1/core_flops_ag$speedup)-(core_flops_ag$cores))\(1 - (1/core_flops_ag$cores)))
-core_flops_ag$karp = (((1/core_flops_ag$speedup)-(1/core_flops_ag$cores))/(1 - (1/core_flops_ag$cores)))
+# Add the karp
+k80_16384_ag$karp = (((1/k80_16384_ag$speedup)-(1/k80_16384_ag$cores))/(1 - (1/k80_16384_ag$cores)))
+p100_16384_ag$karp = (((1/p100_16384_ag$speedup)-(1/p100_16384_ag$cores))/(1 - (1/p100_16384_ag$cores)))
 
-core_flops_format = format.df(core_flops_ag, digits=3)
-core_flops_format = core_flops_format[,-1]
+png("k80_16384_ag.png", width=600, height=200) 
+grid.table(k80_16384_ag, rows=NULL)
+dev.off()
 
-
-# Create box plots for the core_all flops
-# png("core_all.png", width=600, height=500)
-# boxplot(flops~cores, core_all, main="Cores vs FLOPS (10 iterations)", xlab="Cores", 
-# 	ylab="Giga FLOPS", col="light blue")
-# dev.off()
-
-# Save the summary of the perf_28 flops data
-# latex(summary(perf_28$flops), title="28 Cores, 100 iterations")
-
-# latex(core_flops_format)
-
-# Create plot for the karp-flatt metic
-# png("karp_flatt_graph.png")
-# plot(karp~cores, core_flops_ag)
-# dev.off()
-
-
+png("p100_16384_ag.png", width=600, height=200) 
+grid.table(p100_16384_ag, rows=NULL)
+dev.off()
